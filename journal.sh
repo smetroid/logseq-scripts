ddf86f98fd12aa8a26f79801ed51635f3dfb58e4
enriquec <enriquec@vivaldi.net>
2024-11-14 20:15:19 -0700

    add-to-journal

--- journal.sh
+++ journal.sh
@@ -15,6 +15,7 @@ fi
 
 # Accessing the last command in a different way
 CMD=$(HISTFILE=~/.bash_history && history -r && history | tail -n2 | head -n1 | sed 's/^[ ]*[0-9]*[ ]*//')
+echo "CMD: $CMD"
 
 # Get the first word of the CMD as a tag
 TAG=$(echo "$CMD" | cut -d " " -f1)
@@ -35,10 +36,10 @@ TAGS_STRING="${TAGS[*]}"
 
 # Append the formatted content to the journal file
 {
+  echo ""
   echo "- $(date):"
   echo '  ```shell'
   echo "  $CMD"
   echo '  ```'
   echo "  $TAGS_STRING"
-  echo ""
 } >> "$JOURNAL_FILE"
